cab1d69f38be0c8f2f75853315fefafc8ab2c587
Rob Sanchez <rob@toky.com>
2013-01-11 15:17:22 -0600

    fix script loading (onLoad never fired for some reason)

--- oembed/plugin.js
+++ oembed/plugin.js
@@ -13,7 +13,13 @@
         init: function (editor) {
             // Load jquery?
             if (typeof (jQuery) == 'undefined') {
-                CKEDITOR.scriptLoader.load('http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js');
+                CKEDITOR.scriptLoader.load('http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js', function() {
+                    if (typeof (jQuery.fn.oembed) == 'undefined') {
+                        CKEDITOR.scriptLoader.load(CKEDITOR.getUrl(CKEDITOR.plugins.getPath('oembed') + 'libs/jquery.oembed.min.js'));
+                    }
+                });
+            } else if (typeof (jQuery.fn.oembed) == 'undefined') {
+                CKEDITOR.scriptLoader.load(CKEDITOR.getUrl(CKEDITOR.plugins.getPath('oembed') + 'libs/jquery.oembed.min.js'));
             }
             editor.addCommand('oembed', new CKEDITOR.dialogCommand('oembed'));
             editor.ui.addButton('oembed', {
@@ -26,11 +32,6 @@
                     title: editor.lang.oembed.title,
                     minWidth: CKEDITOR.env.ie && CKEDITOR.env.quirks ? 568 : 550,
                     minHeight: 240,
-                    onLoad: function () {
-                        if (typeof (jQuery.fn.oembed) == 'undefined') {
-                            CKEDITOR.scriptLoader.load(CKEDITOR.getUrl(CKEDITOR.plugins.getPath('oembed') + 'libs/jquery.oembed.min.js'));
-                        }
-                    },
                     onCancel: function () {
                         $('#oembedInfoFooter').hide();
                     },

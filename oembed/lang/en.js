4af2bc6ad730ef660f627d33a94fa775605a6eb4
Rob Sanchez <rob@toky.com>
2013-01-21 12:17:05 -0600

    change some descriptions

--- oembed/lang/en.js
+++ oembed/lang/en.js
@@ -1,7 +1,7 @@
 CKEDITOR.plugins.setLang('oembed', 'en', {
-	title : "Embed Media Content (Photo, Video, Audio, Rich)",
+	title : "Embed Video",
     buttonText : "Embed Media Content from Various Sites",
-    pasteUrl : "Paste an URL (Also shortened URLs are supported) from one of the Supported Sites (e.g. YouTube, Flickr, Qik, Vimeo, Hulu, Viddler, MyOpera, etc.) here ...",
+    pasteUrl : "Paste a video URL from YouTube, Vimeo, or Flickr.",
 	width : "Max. Width:",
 	height : "Max. Height:",
 	invalidUrl : "Please provide an valid URL!",
